f8fc069b942936d0d9177c4b92d32cbe248c2a8a
moijrm <moijrm.is.here@gmail.com>
2026-04-15 18:16:46 +0200

    yo yo yo

--- todo.py
+++ todo.py
@@ -1,15 +1,15 @@
 def message():
     return "1. Ajouter tâche\n2. Voir tâche\n3. Supprimer tâche\n4.quitter\n"
-    
+
 tache = []
 numero = 0
-    
+
 while True:
     print(message())
     try:
-        choix = int(input("\033[32m entrer votre choix (1,2,3,4) : "))
+        choix = int(input("entrer votre choix (1,2,3,4) : "))
     
-        
+    
         if choix == 1:
             ecrire = input("Que voulez vous ajoutez : ")
             tache.append(ecrire)
